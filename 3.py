dd5cafd6065acdcb1fc1bafc8c01ee38fb15367e
German <99397105+Eidzhioff@users.noreply.github.com>
2022-02-10 21:08:17 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,7 +0,0 @@
-print("Введите три числа")
-a = int(input("Первое число:"))
-b = int(input("Второе число:"))
-c = int(input("Третье число:"))
-first = a + b
-second = first * c
-print("The answer is", second)
