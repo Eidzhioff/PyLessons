f46a7a36939366698b4465b3a366809ff6315dec
German <99397105+Eidzhioff@users.noreply.github.com>
2022-02-10 21:07:13 +0300

    Add files via upload

---
+++ Hw1/1.py
@@ -0,0 +1,4 @@
+dlina = int(input("Введите длину команты:"))
+shirina = int(input("Введите ширину команты:"))
+ploshad = shirina * dlina
+print("Площаль комнаты состаляет",ploshad,"См^2")
---
+++ Hw1/3.py
@@ -0,0 +1,7 @@
+print("Введите три числа")
+a = int(input("Первое число:"))
+b = int(input("Второе число:"))
+c = int(input("Третье число:"))
+first = a + b
+second = first * c
+print("The answer is", second)
