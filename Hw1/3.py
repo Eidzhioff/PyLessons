586c9e8369174a6a4d4c1cd353a1c21760b5336b
German <99397105+Eidzhioff@users.noreply.github.com>
2022-02-10 21:13:11 +0300

    Update 3.py

--- Hw1/3.py
+++ Hw1/3.py
@@ -1,7 +1,7 @@
 print("Введите три числа")
-a = int(input("Первое число:"))
-b = int(input("Второе число:"))
-c = int(input("Третье число:"))
+a = float(input("Первое число:"))
+b = float(input("Второе число:"))
+c = float(input("Третье число:"))
 first = a + b
 second = first * c
 print("The answer is", second)
